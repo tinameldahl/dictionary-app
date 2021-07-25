ef759f0b79d88199e52b93a4f4a51ec323c557c7
Tina Meldahl <82762154+tinameldahl@users.noreply.github.com>
2021-07-25 17:44:46 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -28,7 +28,7 @@ function Dictionary() {
 
     return (
         <div className="Dictionary">
-            <h1>Dictionary</h1>
+            <h1>Dictionary 1.0</h1>
             <p>Ever wondered what a word means? Search here and find out!</p>
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange} />
