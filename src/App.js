d80164b37d78730e36538bcb2d696c92f7d69423
Tina Meldahl <82762154+tinameldahl@users.noreply.github.com>
2021-07-25 13:17:58 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import logo from "./logo.png";
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello
-        </a>
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <p>Hello</p>
     </div>
   );
 }
