a51aaf14b558736e64a4dca325a0f1d75d3c8c8e
Tina Meldahl <82762154+tinameldahl@users.noreply.github.com>
2021-07-25 15:09:15 +0200

    Showing results and looping through meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
-import Axios from "axios";
+import axios from "axios";
+import Results from "./Results";
 
 import "./Dictionary.css";
-import axios from "axios";
 
 function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response){
-        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function search(event){
         event.preventDefault();
 
+        // Documentation: https://dictionaryapi.dev/
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
-
     }
 
     
     function handleKeywordChange(event){
-        console.log(event.target.value);
         setKeyword(event.target.value);
     }
 
@@ -31,6 +31,7 @@ function Dictionary() {
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange} />
             </form>
+            <Results results={results}/>
         </div>
     ) 
 }
---
+++ src/Meaning.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Meaning(props){
+    console.log(props.meaning);
+    return (
+        <div className="Meaning">
+            <h3>{props.meaning.partOfSpeech}</h3>  
+            <p>Definition: {props.meaning.definitions[0].definition}</p>
+            <p>Example: {props.meaning.definitions[0].example}</p>
+        </div>
+    );
+}
+
+
+export default Meaning;
